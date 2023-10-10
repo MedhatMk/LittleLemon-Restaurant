5657c8812c447ed3d8d9f996440cb8178dffdb44
Medhat <matouk7@outlook.com>
2023-10-10 13:26:10 +0200

    .

--- LittleLemon/Restaurant/urls.py
+++ LittleLemon/Restaurant/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-
     # add more URL patterns here
+    path('menu/',views.MenuItems.as_view()),
+    path('menu/<int:pk>', views.SingleMenuItem.as_view())
 ]
--- LittleLemon/Restaurant/views.py
+++ LittleLemon/Restaurant/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
-
+from rest_framework import generics
+from .models import Menu
+from .serializers import MenuSerializer
 # Create your views here.
+
+class MenuItems(generics.ListAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+class SingleMenuItem(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
